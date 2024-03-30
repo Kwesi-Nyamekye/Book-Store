1cee7531e1aaff03d28206dbb5bc0eabf8477ec8
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-03-30 23:38:41 +0000

    Add templates to app

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+def index (request):
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+def book_detail(request, id):
+    return render(request, "book_outlet/book_detail.html" )
