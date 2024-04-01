b70d1db5533d5e3f2a0842d42aaad76134848946
Kwesi Nyamekye <nanakwesiwalker2016@gmail.com>
2024-04-01 00:27:07 +0000

    Add views to book_outlet and details page

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,13 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.db.models import Avg
 
 # Create your views here.
 
 def index (request):
-    books = Book.objects.all()
-    return render(request, "book_outlet/index.html", {
-        "books": books
-    })
+    books = Book.objects.all().order_by("rating")
+    number_of_books = books.count()
+    avg_rating = books.aaggregate(Avg("rating"))
 
-def book_detail(request, id):
-    return render(request, "book_outlet/book_detail.html" )
+    return render(request, "book_outlet/index.html", {
+        "books": books,
+        "total_number_of_books": number_of_books,
+        "average_rating": avg_rating
+          })
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, "book_outlet/book_detail.html",{
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.Is_bestselling
+    } ) 
